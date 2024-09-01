042ee195bb68ddeced26d583ff386995225356ee
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2024-09-01 18:20:29 +0330

    Update craprot pdf.py

--- craprot pdf.py
+++ craprot pdf.py
@@ -1,25 +1,32 @@
+# modules : PyPDF2 , tkinter.filedialog
+# PyPDF2 : install this using -> pip install PyPDF2
+# tkinter.filedialog : exists in python as default
+# this code read some pages of a pdf and extract they context and insert them in to other PDF file 
+
 from PyPDF2 import PdfReader, PdfWriter
 from tkinter.filedialog import askopenfilename
 
-def extract_pages(input_pdf_path, start_page, end_page, output_pdf_path):
+def extract_pages(input_pdf_path, start_page, end_page, output_pdf_path): # using this function extract the pages
     
-    reader = PdfReader(input_pdf_path)
-    
-    
-    writer = PdfWriter()
+    reader = PdfReader(input_pdf_path) # read PDF
     
     
-    for page_num in range(start_page - 1, end_page):
+    writer = PdfWriter() # the output class
+    
+    
+    for page_num in range(start_page - 1, end_page): # read pages
         if page_num < len(reader.pages):
-            writer.add_page(reader.pages[page_num])
+            writer.add_page(reader.pages[page_num]) 
     
     
     with open(output_pdf_path, 'wb') as output_pdf:
-        writer.write(output_pdf)
+        writer.write(output_pdf) # write on output
     
-    print(f"Pages {start_page} to {end_page} have been extracted and saved to {output_pdf_path}")
+    print(f"Pages {start_page} to {end_page} have been extracted and saved to {output_pdf_path}") # show the user the resute
 
 
+
+# get inputs and call the functions 
 input_pdf = askopenfilename()
 start_page = 1  
 end_page = int(input("Enter your pagenum: "))    
